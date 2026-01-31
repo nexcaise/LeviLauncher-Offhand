5b429cda0b63009b839757ef5677a664b1875031
alvinqid <github@alvinq.my.id>
2026-02-01 05:45:26 +0700

    test

--- src/ItemRegistry.hpp
+++ src/ItemRegistry.hpp
@@ -5,8 +5,24 @@
 #include "Item.hpp"
 #include <vector>
 
+class StringHash {
+public:
+    int mHash;
+    bool operator==(StringHash const& o) const { return mHash == o.mHash; }
+};
+
+namespace std {
+template<>
+struct hash<StringHash> {
+    size_t operator()(StringHash const& s) const noexcept {
+        return std::hash<int>()(s.mHash);
+    }
+};
+}
+
 class ItemRegistry {
 public:
     std::vector<SharedPtr<Item>> mItemRegistry;
     std::unordered_map<int, WeakPtr<Item>> mIdToItemMap;
+    std::unordered_map<StringHash, WeakPtr<Item>> mNameToItemMap;
 };
