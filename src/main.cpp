6a43a2b8accbe1f354a81cd83b4013582f127863
alvinqid <github@alvinq.my.id>
2026-02-03 17:25:42 +0700

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,98 +1,71 @@
-#include "features/Offhand.hpp"
 #include "pl/Gloss.h"
-#include <jni.h>
-//#include <android/log.h>
-#include <dlfcn.h>
-#include <unistd.h>
-#include <cstring>
-#include <string>
+#include <dobby.h>
+#include "pl/Signature.h"
+#include "pl/Logger.h"
+//#include "pl/internal/macro.h"
+#include "util/InteractionResult.hpp"
+#include "world/item/ItemStack.hpp"
+#include "world/item/Item.hpp"
 
-//#include <dobby.h>
+pl::log::Logger omlogger("OffhandMod");
 
-//#include "pl/Signature.h"
-
-//#include "util/ItemRegistry.hpp"
-//#include "util/ItemRegistryRef.hpp"
-//#include "util/VanillaItems.hpp"
-
-//#include <thread>
-//#include <chrono>
-
-//#define LOG_TAG "HmmAja"
-//#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
-//#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
-/*
-class BaseGameVersion;
-class Experiments;
-class VanillaItems;
-namespace cereal { struct ReflectionCtx; }
-
-using TargetFn = void(*)(
-        VanillaItems*,
-        cereal::ReflectionCtx&,
-        ItemRegistryRef,
-        BaseGameVersion const&,
-        Experiments const&
+using useItemOnFn = InteractionResult* (*)(
+        void*,
+        ItemStack&,
+        void*,
+        void*,
+        void*,
+        void*,
+        bool
 );
 
-static TargetFn orig = nullptr;
+static useItemOnFn useItemOn_orig = nullptr;
 
-void hook(
-        VanillaItems* self,
-        cereal::ReflectionCtx& ctx,
-        ItemRegistryRef itemRegistry,
-        BaseGameVersion const& baseGameVersion,
-        Experiments const& experiments
+InteractionResult* useItemOn_hook(
+        void* self,
+        ItemStack& stack,
+        void* at,
+        void* face,
+        void* hit,
+        void* tb,
+        bool isFirstEvent
 ) {
-    LOGI("Hook::Start");
-    if (orig) {
-        LOGI("Orig();");
-        orig(self, ctx, itemRegistry, baseGameVersion, experiments);
-    }
-  
-    std::this_thread::sleep_for(std::chrono::seconds(10));
-    
-    auto sp = itemRegistry._lockRegistry();
-    if (!sp) {
-        LOGI("Registry expired");
-        LOGI("Hook::End");
-        return;
-    }
-    
-    for (auto& pair : sp.get()->mIdToItemMap)
+    Item* item = stack.getItem();
+    bool realMayUse = stack.mItem == nullptr;//!stack.mValid || stack.mItem == nullptr || stack.isNull() ||/* stack.mCount == 0 || !isSimTick ||*/ !item/* || item->canUseOnSimTick()*/;
+
+    if (!realMayUse)
     {
-        pair.second.get()->setAllowOffhand(true);
+        omlogger.info("May not use item");
+        return useItemOn_orig(self,stack,at,face,hit,tb,isFirstEvent);
     }
     
-    LOGI("Hook::End");
+    omlogger.info("item.setAllowOffhand(true);");
+    item->setAllowOffhand(true);
+
+    return useItemOn_orig(self,stack,at,face,hit,tb,isFirstEvent);
+
 }
-*/
+
 __attribute__((constructor))
 void Init() {
     GlossInit(true);
-    /*
-    uintptr_t addr = pl::signature::pl_resolve_signature(
-        //1.21.124
-        //"FD 7B BA A9 FC 6F 01 A9 FA 67 02 A9 F8 5F 03 A9 F6 57 04 A9 F4 4F 05 A9 FD 03 00 91 FF 07 40 D1 FF 83 00 D1 48 D0 3B D5 EA 83 00 B2"
-        //1.21.132
-        "FD 7B BA A9 FC 6F 01 A9 FA 67 02 A9 F8 5F 03 A9 F6 57 04 A9 F4 4F 05 A9 FD 03 00 91 FF 07 40 D1 FF 03 01 D1 48 D0 3B D5"
-        ,
+    uintptr_t useItemOn_addr = pl::signature::pl_resolve_signature(
+        "EC 13 40 F9 E8 03 16 AA",
         "libminecraftpe.so"
     );
-    if (!addr) {
-        LOGE("Signature not found");
+    if (!useItemOn_addr) {
+        omlogger.error("Signature not found");
         return;
     }
-    LOGI("Signature found at: %p", (void*)addr);
-    int ret = DobbyHook(
-        (void*)addr,
-        (void*)hook,
-        (void**)&orig
+    omlogger.info("Signature found at: {}", (void*)useItemOn_addr);
+    int useItemOn_ret = DobbyHook(
+        (void*)useItemOn_addr,
+        (void*)useItemOn_hook,
+        (void**)&useItemOn_orig
     );
-    if (ret == 0) {
-        LOGI("DobbyHook success");
+    if (useItemOn_ret == 0) {
+        omlogger.info("DobbyHook success");
     } else {
-        LOGE("DobbyHook failed: %d", ret);
-    }*/
-    OffhandHooks();
+        omlogger.error("DobbyHook failed: {}", useItemOn_ret);
+    }
 }
