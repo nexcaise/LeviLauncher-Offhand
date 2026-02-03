7aa4da9d5a33d4f6cd69e23383f53170770d8ff2
alvinqid <github@alvinq.my.id>
2026-02-03 20:09:21 +0700

    test

--- src/world/item/ItemStack.hpp
+++ src/world/item/ItemStack.hpp
@@ -1,11 +1,10 @@
 #pragma once
 #include "world/item/Item.hpp"
 #include "world/item/ItemStackBase.hpp"
-#include "util/SharedPtr.hpp"
 
 class ItemStackNetIdVariant;
 
 class ItemStack : public ItemStackBase {
 public:
-    ItemStackNetIdVariant mNetIdVariant;
+    ItemStackNetIdVariant* mNetIdVariant;
 };
