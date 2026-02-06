bea6c7df07122c2be1d38fc07319ce9547e2d6f0
alvinqid <github@alvinq.my.id>
2026-02-06 18:49:12 +0700

    test

--- src/features/Hooks.hpp
+++ src/features/Hooks.hpp
@@ -1,5 +1,5 @@
 #include "world/item/registry/ItemRegistryRef.hpp"
-#include "world/item/registry/Item.hpp"
+#include "world/item/Item.hpp"
 #include "world/item/registry/ItemRegistry.hpp"
 
 void RegisterHooks();
